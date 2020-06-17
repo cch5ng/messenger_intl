5e48b65b596beeb1c45c94365894b77f51d53171
Carol Chung <cch5ng@gmail.com>
2020-06-17 12:03:16 -0700

    stub out logic to format data required in summary display

--- client/src/components/Sidebar/ChatSummary/ChatSummaryItem.js
+++ client/src/components/Sidebar/ChatSummary/ChatSummaryItem.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import {useAuth} from '../../../context/auth-context';
+
 const ChatSummaryItem = (props) => {
-  
+  const {conversation} = props;
+  const {user} = useAuth();
+
+  let lastMessage = conversation.messages[0];
+
+  const filterSelfEmail = (emailAr) => {
+    let selfIdx = emailAr.indexOf(user.email);
+    let copyUserEmails = emailAr.slice(0);
+    copyUserEmails.splice(selfIdx, 1);
+    return copyUserEmails.join(', ');
+  }
+
+  const getLastMessageTranslated = () => {
+    if (lastMessage.language === user.language) {
+      return lastMessage.original_message;
+    } else {
+      return lastMessage.translations[user.language];
+    }
+  }
+
   return (
-    <div>TODO</div>
+    <div>
+      <p>avatar</p>
+      <p>{filterSelfEmail(conversation.user_emails)}</p>
+      <p>{getLastMessageTranslated()}</p>
+      <p>unread messages count</p>
+    </div>
   )
 }
 

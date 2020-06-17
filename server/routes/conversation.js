cc673b324e3f5148d2569dc2928db8e0ab06b553
Carol Chung <cch5ng@gmail.com>
2020-06-17 10:16:38 -0700

    comment out some old logic in GET conversations for email route; could not remember reason for building uid dict

--- server/routes/conversation.js
+++ server/routes/conversation.js
@@ -44,42 +44,43 @@ router.post("/",
     {uid_as_string: {email, language}}
     as well as the conversations
 */
-router.get("/user/:id",
+router.get("/user/:email",
     passport.authenticate('jwt', { session: false }),
     function(req, res, next) {
-        const {id} = req.params;
+        const {email} = req.params;
         let objId = mongoose.Types.ObjectId;
-        Conversation.find({users:{ "$in" : [new objId(id)]} })
+        Conversation.find({user_emails:{ "$in" : [email]} })
             .sort({updated_on: -1})
             .exec(function(err, conversations) {
                 if (err) {
                     return handleError(err);
                 }
                 if (conversations && conversations.length) {
-                    let dictUidToObj = {};
-                    conversations.forEach((conversation, idx) => {
-                        conversation.users.forEach((uid, idx2) => {
-                            let innerIdx = idx2;
-                            if (!dictUidToObj[uid.toString()]) {
-                                User.findById(uid.toString(), function(err, user) {
-                                    if (err) {
-                                        return handleError(err)
-                                    }
-                                    if (user) {
-                                        let userObj = {};
-                                        userObj.email = user.email;
-                                        userObj.language = user.language;
-                                        dictUidToObj[uid.toString()] = userObj;
-                                        if (idx === conversations.length - 1 && innerIdx === conversation.users.length - 1) {
-                                            res.json({ type: "success", conversations, dictUidToObj})
-                                        }                
-                                    }
-                                })
-                            }
-                        })
-                    })
+                    //let dictUidToObj = {};
+                    res.json({ type: "success", conversations})
+                    // conversations.forEach((conversation, idx) => {
+                    //     conversation.users.forEach((uid, idx2) => {
+                    //         let innerIdx = idx2;
+                    //         if (!dictUidToObj[uid.toString()]) {
+                    //             User.findById(uid.toString(), function(err, user) {
+                    //                 if (err) {
+                    //                     return handleError(err)
+                    //                 }
+                    //                 if (user) {
+                    //                     let userObj = {};
+                    //                     userObj.email = user.email;
+                    //                     userObj.language = user.language;
+                    //                     dictUidToObj[uid.toString()] = userObj;
+                    //                     if (idx === conversations.length - 1 && innerIdx === conversation.users.length - 1) {
+                    //                         res.json({ type: "success", conversations, dictUidToObj})
+                    //                     }                
+                    //                 }
+                    //             })
+                    //         }
+                    //     })
+                    // })
                 } else {
-                    res.json({ type: "success", message: "There are no current conversations"})
+                    res.json({ type: "success", conversations: [], message: "There are no current conversations"})
                 }
             }
         )

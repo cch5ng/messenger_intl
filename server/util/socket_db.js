5060de4cd1f47d931727ddd4eceac2748773ad8b
Carol Chung <cch5ng@gmail.com>
2020-06-19 19:59:28 -0700

    add helpers, invitationsPopulated(), invitationsPopulated()

--- server/util/socket_db.js
+++ server/util/socket_db.js
@@ -15,4 +15,35 @@ const saveMessageToConversation = ({message, updated_on, conversationId}) => {
                 })
 }
 
-module.exports = {saveMessageToConversation};
+const getUniquePairsFromGroup = (emails) => {
+  if (emails.length <=2 ) {
+    return emails;
+  }
+  let emailPairs = [];
+  for (let i = 0; i < emails.length - 1; i++) {
+    for (let j = i + 1; j < emails.length; j++) {
+      let pair = [emails[i], emails[j]];
+      emailPairs.push(pair);
+    }
+  }
+  return emailPairs;
+}
+
+//@param [string]
+const buildQueryIsPairFriends = (emails) => {
+  let query = {};
+  let queryAr = [];
+  let queryObj1 = {};
+  let queryObj2 = {};
+  queryObj1.to_user_email = emails[0];
+  queryObj1.from_user_email = emails[1];
+  queryAr.push(queryObj1);
+  queryObj2.to_user_email = emails[1];
+  queryObj2.from_user_email = emails[0];
+  queryAr.push(queryObj2);
+  query['$or'] = queryAr;
+  query.approved = true;
+  return query;
+}
+
+module.exports = {saveMessageToConversation, getUniquePairsFromGroup, buildQueryIsPairFriends};

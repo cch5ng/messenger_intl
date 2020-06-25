28718de598e02b19759a4b049f48070f9880e3e8
Carol Chung <cch5ng@gmail.com>
2020-06-25 13:18:01 -0700

    stub out logic for conversation data store; initConversationsAr(), setClientSocketListener() - not working, initConversationsDict(), addMessageToConversation(), useEffect()

--- client/src/context/socket-context.js
+++ client/src/context/socket-context.js
@@ -9,22 +9,75 @@ function SocketProvider({children}) {
   const [conversationsAr, setConversationsAr] = useState([]);
   const [curConversationId, setCurConversationId] = useState(null);
   const [conversationsColorsDict, setConversationsColorsDict] = useState({});
-
-  //TODO
-  const addMessageToConversation = ({conversationId, message}) => {
-
-  }
-
-  //should happen once per session, on initConversationsAr
-  const initConversationsDict = () => {
-
-  }
+  let conversationIdToArIdxDict = {}; //used to update the conversationsAr quickly on new messages
 
   //should happen once per session
   const initConversationsAr = (conversations) => {
-    setConversationsAr(conversations);
+    if (conversations.length) {
+      setConversationsAr(conversations);
+      conversations.forEach((convo, idx) => {
+        let id = convo._idx.toString();
+        conversationIdToArIdxDict[id] = idx;
+        //TODO and TEST
+        //setClientSocketListener(convo._idx);
+      })
+      console.log('conversationIdToArIdxDict', conversationIdToArIdxDict)
+      //TODO verify timing of this, is setConversationsAr ever too slow to init the ConversationsDict?
+      initConversationsDict(conversations);  
+    }
   }
 
+  //TODO TEST, verify the data format of data.message
+  const setClientSocketListener = (conversationId) => {
+    //socket listener for incoming messages
+    socket.on(conversationId, (data) => {
+      console.log('new incoming message', data)
+      setConversationsDict({...conversationsDict,
+        conversationId: {...conversationsDict[conversationId], 
+          messages: conversationsDict[conversationId]['messages'].concat([data.message])
+        }
+      })
+      //TODO update the conversationsAr with new dict data
+    });
+  }
+
+  //TODO TEST
+  //should happen once per session, on initConversationsAr
+  const initConversationsDict = (convoAr) => {
+    if (convoAr.length) {
+      let dict = {};
+      convoAr.forEach(convo => {
+        //verify whether need to convert _id value toString
+        let id = convo._id.toString();
+        dict[id] = convo;
+      });
+      setConversationsDict(dict);
+    }
+  }
+
+  //TODO TEST
+  const addMessageToConversation = ({conversationId, message}) => {
+    console.log('conversationsDict', conversationsDict)
+    console.log('conversationId', conversationId)
+    console.log('type conversationId', typeof conversationId)
+    setConversationsDict({
+      ...conversationsDict,
+      [conversationId]: {
+        ...conversationsDict[conversationId],
+        messages: conversationsDict[conversationId]['messages'].concat([message])
+      }
+    })
+  }
+  
+  const sendChatMessage = ({from_email, message, conversationId, userEmails, friendLanguages, action}) => {
+    socket.send({message, conversationId, userEmails, friendLanguages, action});
+  }
+
+  const sendGroupChatInitMessage = ({from_email, message, conversationId, userEmails, action}) => {
+    socket.send({message, conversationId, userEmails, action});
+  }
+
+  //TODO resolve data storage when user goes to /conversations/:conversationId, do I update the conversationsDict with BE resp?
   const getConversationById = () => {
 
   }
@@ -48,20 +101,29 @@ function SocketProvider({children}) {
     setConversationsAr([conversation].concat(conversationsAr));
   }
 
-  const sendChatMessage = ({from_email, message, conversationId, userEmails, friendLanguages, action}) => {
-    socket.send({message, conversationId, userEmails, friendLanguages, action});
-  }
-
-  const sendGroupChatInitMessage = ({from_email, message, conversationId, userEmails, action}) => {
-    socket.send({message, conversationId, userEmails, action});
-  }
-
+  //TODO and TEST
+  useEffect(() => {
+    console.log('gets to socket context useEffect')
+    //should update conversationsAr
+    if (Object.keys(conversationsDict).length) {
+      let newConversationsAr = [];
+      console.log('conversationIdToArIdxDict', conversationIdToArIdxDict)
+      Object.keys(conversationIdToArIdxDict).forEach(conversationId => {
+        let arIdx = conversationIdToArIdxDict[conversationId];
+        console.log('arIdx', arIdx)
+        newConversationsAr[arIdx] = conversationsDict[conversationId];
+      });
+      console.log('newConversationsAr', newConversationsAr);
+      setConversationsAr(newConversationsAr);      
+    }
+  }, [conversationsDict]);
 
   const socketShare = {socket,
                       conversationsAr,
                       sendChatMessage,
                       addMessageToConversation, 
                       initConversationsAr,
+                      initConversationsDict,
                       curConversationId,
                       addConversationColor,
                       getConversationColorById,

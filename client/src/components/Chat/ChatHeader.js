e5a32c970c797b5a9944b7313e7e13b076d7022e
Carol Chung <cch5ng@gmail.com>
2020-06-30 16:23:22 -0700

    pass color into ChatHeader

--- client/src/components/Chat/ChatHeader.js
+++ client/src/components/Chat/ChatHeader.js
@@ -5,6 +5,12 @@ import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Switch from '@material-ui/core/Switch';
 import { deepOrange, deepPurple } from '@material-ui/core/colors';
+import pink from '@material-ui/core/colors/pink';
+import indigo from '@material-ui/core/colors/indigo';
+import amber from '@material-ui/core/colors/amber';
+import teal from '@material-ui/core/colors/teal';
+import red from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,6 +26,30 @@ const useStyles = makeStyles((theme) => ({
   purple: {
     color: theme.palette.getContrastText(deepPurple[500]),
     backgroundColor: deepPurple[500],
+  },
+  pink: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: pink[500],
+  },
+  indigo: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: indigo[500],
+  },
+  amber: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: amber[500],
+  },
+  teal: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: teal[500],
+  },
+  red: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: red[500],
+  },
+  green: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: green[500],
   },
   chatHeaderName: {
     padding: '0 5px',
@@ -62,6 +92,7 @@ const AntSwitch = withStyles((theme) => ({
 
 const ChatHeader = props => {
   let friendEmails = props && props.friendEmails.length ? props.friendEmails : [];
+  let {color} = props;
   const classes = useStyles();
 
   let initial = friendEmails && friendEmails.length === 1 ? friendEmails[0][0].toUpperCase() : '';
@@ -90,7 +121,12 @@ const ChatHeader = props => {
       <div className="chatHeaderLeft">
         {friendEmails.length === 1 && (
           <Fragment>
-            <Avatar className={classes.orange}>{initial}</Avatar>
+            {color && (
+              <Avatar className={classes[color]}>{initial}</Avatar>
+            )}
+            {!color && (
+              <Avatar>{initial}</Avatar>
+            )}
             <Typography variant='h5' className={classes.chatHeaderName}>{friendEmails[0].split('@')[0]}</Typography>
           </Fragment>
         )}

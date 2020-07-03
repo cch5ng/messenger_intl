239cf05b5868a874d31941c38a6ef1c489dc8d7a
Carol Chung <cch5ng@gmail.com>
2020-07-03 15:56:31 -0700

    remove getEmailSendSuccessMessage() bc it was similar to getInviteSendSuccessMessage()

--- server/util/index.js
+++ server/util/index.js
@@ -1,7 +1,3 @@
-const getEmailSendSuccessMessage = (emails) => {
-  return `Email invitations were sent to ${emails.join(', ')}`;
-}
-
 const getEmailSendMixedMessage = (sentEmails, unsentEmails) => {
   let inviteNotCreatedEmailMessage = `Invitations were not sent to ${unsentEmails.join(', ')} because the invitation was already sent, pending, or rejected.`;
   let inviteCreatedEmailMessage = `Invitations were sent to ${sentEmails.join(', ')}.`;
@@ -17,4 +13,4 @@ const getInviteNotSentMessage = (emails) => {
   return `Invitations were not sent to ${emails.join(', ')} because the invitation was already sent, pending, or rejected.`;
 }
 
-module.exports = {getEmailSendSuccessMessage, getEmailSendMixedMessage, getInviteSendSuccessMessage, getInviteNotSentMessage};
+module.exports = {getEmailSendMixedMessage, getInviteSendSuccessMessage, getInviteNotSentMessage};

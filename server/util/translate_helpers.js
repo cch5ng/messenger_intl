c1e5bc1254d6bba433c230c98228dd3530a1150f
Carol Chung <cch5ng@gmail.com>
2020-07-13 18:25:24 -0700

    try to change path to credential file on prod

--- server/util/translate_helpers.js
+++ server/util/translate_helpers.js
@@ -2,7 +2,12 @@ const {Translate} = require('@google-cloud/translate').v2;
 const GOOGLE_APPLICATION_CREDENTIALS = require('../translation-credentials.json');
 
 const {project_id} = GOOGLE_APPLICATION_CREDENTIALS;
-const keyFilename = '../server/translation-credentials.json';
+let keyFilename;
+if (process.env.NODE_ENV === 'production') {
+  keyFilename = '../translation-credentials.json';
+} else if (process.env.NODE_ENV === 'development') {
+  keyFilename = '../server/translation-credentials.json';
+}
 
 const translate = new Translate({projectId: project_id, keyFilename});
 

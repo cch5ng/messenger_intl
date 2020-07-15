1b2658683d0565e9fdff84b89d072946c30cce06
Carol Chung <cch5ng@gmail.com>
2020-07-15 16:03:14 -0700

    update chat credentials path for prod (#85)

--- server/util/translate_helpers.js
+++ server/util/translate_helpers.js
@@ -2,7 +2,9 @@ const {Translate} = require('@google-cloud/translate').v2;
 const GOOGLE_APPLICATION_CREDENTIALS = require('../translation-credentials.json');
 
 const {project_id} = GOOGLE_APPLICATION_CREDENTIALS;
-const keyFilename = '../server/translation-credentials.json';
+let keyFilename = process.env.NODE_ENV === 'production'
+                  ? '../translation-credentials.json'
+                  : '../server/translation-credentials.json';
 const translate = new Translate({projectId: project_id, keyFilename});
 
 async function getTranslation(text, target) {

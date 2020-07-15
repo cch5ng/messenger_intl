edb429ad8fd18e5b3057b98b4c8d5e60da11c44f
Carol Chung <cch5ng@gmail.com>
2020-07-15 13:19:01 -0700

    update node debug option

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ "$ENABLE_DEBUG" == "true" ]; then
 	echo "Starting with debugger on port $DEBUG_PORT"
-	exec with_ngrok node --debug=$DEBUG_PORT ./server/bin/www
+	exec with_ngrok node --inspect=$DEBUG_PORT ./server/bin/www
 else
 	echo "Starting without debugger"
 	exec node ./server/bin/www

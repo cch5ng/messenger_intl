1a7541e31dde46a3756fde9bf705611592097a52
Carol Chung <cch5ng@gmail.com>
2020-07-15 16:37:49 -0700

    update path to credentials on prod

--- server/util/translate_helpers.js
+++ server/util/translate_helpers.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const {Translate} = require('@google-cloud/translate').v2;
 const GOOGLE_APPLICATION_CREDENTIALS = require('../translation-credentials.json');
 
 const {project_id} = GOOGLE_APPLICATION_CREDENTIALS;
+console.log('dirname', __dirname)
 let keyFilename = process.env.NODE_ENV === 'production'
-                  ? '../translation-credentials.json'
+                  ? './server/translation-credentials.json'
                   : '../server/translation-credentials.json';
 const translate = new Translate({projectId: project_id, keyFilename});
 

329846d09f6c03cb419dd58f20a861b760648d4a
Carol Chung <cch5ng@gmail.com>
2020-07-15 14:59:44 -0700

    82 debug config5 (#83)
    * update node debug option
    
    * removed obsolete file
    
    * revert to old way of starting server

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$ENABLE_DEBUG" == "true" ]; then
-	echo "Starting with debugger on port $DEBUG_PORT"
-	exec with_ngrok node --debug=$DEBUG_PORT ./server/bin/www
-else
-	echo "Starting without debugger"
-	exec node ./server/bin/www
-fi

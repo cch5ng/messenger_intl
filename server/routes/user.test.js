fb97ddecb91d289edbe237197bafcdc290dccb31
Carol Chung <cch5ng@gmail.com>
2020-07-25 17:36:45 -0700

    add tests for login route

--- server/routes/user.test.js
+++ server/routes/user.test.js
@@ -1,10 +1,9 @@
 let request = require('supertest');
 request('http://127.0.0.1:3001');
 const app = require("../app.js");
-//const { expect } = require('chai');
 const User = require('../models/user');
 
-describe('User API endpoints', () => {
+describe('User API register endpoint', () => {
 
   afterAll((done) => {
     jest.setTimeout(30000);
@@ -92,6 +91,92 @@ describe('User API endpoints', () => {
         "language": "english"
       })
       .expect(201, done)
+  });
+
+});
+
+describe('User API login endpoints', () => {
+
+  beforeAll((done) => {
+    jest.setTimeout(30000);
+    request(app)
+    .post('/user/register')
+    .set('Content-Type', 'application/json')
+    .send({"email": "test999@t.com",
+      "password": "testPassword10&",
+      "confirmPassword": "testPassword10&",
+      "language": "english"
+    })
+    .then(() => done())
+  })
+
+  afterAll((done) => {
+    jest.setTimeout(30000);
+
+    User.findOneAndDelete({"email": "test999@t.com"}, function(err, doc) {
+      if (err) {
+        console.error('err', err);
+        done()
+      }
+      if (doc) {
+        done()
+      }
+    })
+  });
+
+  test('should test POST /login reject user with no registered', (done) => {
+    request(app)
+      .post('/user/login')
+      .set('Content-Type', 'application/json')
+      .send({"email": "test9999@t.com",
+        "password": "test"
+      })
+      .expect(404, done)
+  });
+
+  test('should test POST /login empty email', (done) => {
+    request(app)
+      .post('/user/login')
+      .set('Content-Type', 'application/json')
+      .send({"email": "",
+        "password": "testPassword10&"
+      })
+      .expect(400, done)
+  });
+
+  test('should test POST /login valid email', (done) => {
+    request(app)
+      .post('/user/login')
+      .set('Content-Type', 'application/json')
+      .send({"email": "invalid@",
+        "password": "testPassword10&"
+      })
+      .expect(400, done)
+  });
+
+  test('should test POST /login empty password', (done) => {
+    request(app)
+      .post('/user/login')
+      .set('Content-Type', 'application/json')
+      .send({"email": "test999@t.com",
+        "password": ""
+      })
+      .expect(400, done)
+  });
+
+  test('should test POST /login able to login successfully', (done) => {
+    jest.setTimeout(30000);
+    request(app)
+      .post('/user/login')
+      .set('Content-Type', 'application/json')
+      .send({"email": "test999@t.com",
+        "password": "testPassword10&"
+      })
+      .expect(200)
+      .then(resp => {
+        expect(resp.body.token.length).not.toBe(0);
+        done();
+      })
   });
 
 });

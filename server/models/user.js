cbcbbaea25c7fbbbe1bf0a8e1d98cac15a2107e5
Carol Chung <cch5ng@gmail.com>
2021-07-26 15:51:43 -0700

    update user model for password url and password url expir date

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema( {
+const userSchema = new Schema({
   email: {
     type: String,
     required: true
@@ -18,6 +18,14 @@ const userSchema = new Schema( {
   referral_id: {
     type: mongoose.Types.ObjectId,
     default: mongoose.Types.ObjectId()
+  },
+  password_change_url_id: {
+    type: mongoose.Types.ObjectId,
+    default: mongoose.Types.ObjectId()
+  },
+  password_change_expiration_date_time: { 
+    type: Date, 
+    default: null 
   }
 });
 

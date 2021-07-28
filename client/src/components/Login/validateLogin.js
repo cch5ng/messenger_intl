b94212e7729ba31914ca60263fef01d8dd3f6af3
Carol Chung <cch5ng@gmail.com>
2021-07-28 10:41:05 -0700

    stub out separate helpers for validation of password change forms but still need to refactor to do exports

--- client/src/components/Login/validateLogin.js
+++ client/src/components/Login/validateLogin.js
@@ -20,4 +20,40 @@ const validateLogin = formValues => {
   return formErrors;
 };
 
+const validateEmailForPasswordChange = formValues => {
+  let formErrors = {};
+  const { email} = formValues;
+  if(validator.isEmpty(email)) {
+    formErrors.email = 'Email is required';
+  }
+  else if(!validator.isEmail(email)) {
+    formErrors.email = 'Email entered is invalid';
+  }
+
+  return formErrors;
+};
+
+const validatePasswordForPasswordChange = formValues => {
+  let formErrors = {};
+  const { password, passwordConfirm } = formValues;
+
+  if(validator.isEmpty(password)) {
+    formErrors.password = 'Password is required';
+  } else if(!validator.isLength(password, {min: 6})) {
+    formErrors.password = 'Password should have at least 6 characters';
+  }
+
+  if(validator.isEmpty(passwordConfirm)) {
+    formErrors.passwordConfirm = 'Confirmation password is required';
+  } else if(!validator.isLength(passwordConfirm, {min: 6})) {
+    formErrors.passwordConfirm = 'Confirmation password should have at least 6 characters';
+  }
+
+  if (password !== passwordConfirm) {
+    formErrors.passwordMatch = 'The password and confirmation passwords must match.'
+  }
+
+  return formErrors;
+};
+
 export default validateLogin;

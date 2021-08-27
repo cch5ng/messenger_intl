0d948751a75d22c06b201c1c7769f259740701c6
Carol Chung <cch5ng@gmail.com>
2021-08-27 16:20:06 -0700

    cleanup

--- client/src/components/Chat/MessageInput.js
+++ client/src/components/Chat/MessageInput.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React from 'react';
 import Grid from "@material-ui/core/Grid";
 import InputBase from '@material-ui/core/InputBase';
 import { makeStyles } from '@material-ui/core/styles';
